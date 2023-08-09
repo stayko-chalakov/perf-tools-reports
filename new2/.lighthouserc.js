106610201f9db3ae1426c3abd1d9ecb0a7db000b
Stayko Chalakov <stayko.chalakov@gocity.com>
2023-08-09 21:42:53 +0100

    feat: more reports

---
+++ new2/.lighthouserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ci: {
+    collect: {
+      isSinglePageApplication: false,
+      numberOfRuns: 1,
+      headful: false,
+      url: ['https://gocity.com/en-us'],
+      psiStrategy: 'mobile',
+    },
+  },
+};
